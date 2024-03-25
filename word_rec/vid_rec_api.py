20c04fbc119125b6a6ad77c58e6cb7ebd8831fb5
Dylan Moraes <dylanmoraesdljdd@gmail.com>
2024-03-25 11:46:23 +0530

    api

--- word_rec/vid_rec_api.py
+++ word_rec/vid_rec_api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
-from schemas.api_class import (word, translate)
+from api_class import articles
 from fastapi import FastAPI, Request, Form, status, Body, Response, BackgroundTasks
 from fastapi.responses import JSONResponse
 from word_rec_fun import return_vid_recs
@@ -8,6 +8,6 @@ from word_rec_fun import return_vid_recs
 app = FastAPI()
 
 @app.post("/get_vid_recs_from_score")
-async def articles(body: word):
-    output = await return_vid_recs(body.user_query)
+async def articles(body: articles):
+    output = await return_vid_recs(body.user_dict)
     return {"output":output}
